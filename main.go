eb2a85a4e374d04fcab0dbe7135feac671c559c6
protoappxyz <aars0001@agonquinlive.com>
2024-04-21 18:24:57 -0400

    Now we trigger a redeployment upon image push

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "ponging"})
+		c.JSON(http.StatusOK, gin.H{"message": "pongingtest"})
 	})
 
 	r.GET("/health", func(c *gin.Context) {
